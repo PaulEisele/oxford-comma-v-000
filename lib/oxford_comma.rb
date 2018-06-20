6d359ac7fef84e93a59e0bb748ec5db84927d87d
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-20 01:17:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-  array<<array.split(",")
+if array.count == 1
+end
+array.join
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    final = array.pop
+    array.join(", ") << ", and #{final}"
+end
 end
